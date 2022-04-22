892448a9426193394220e54fdcdc660d2e176705
marcosfp <marcosfppf@gmail.com>
2022-04-22 17:04:51 +0200

    eliminado token

--- telebot_avisos.py
+++ telebot_avisos.py
@@ -12,10 +12,6 @@ def enviar_mensaje(message):
     token = os.environ.get('token')
     user_telegram = os.environ.get('user_telegram')
 
-    api_id = '17143890'
-    api_hash = '45143b37aa797e9dc15a0c7fdd027bbe'
-    token = '5202593674:AAFbeOu2OcuQ0Edjpe-Aq2UMuknLPD0Pp-I'
-
     # Create bot
     bot = telebot.TeleBot(token)
 
